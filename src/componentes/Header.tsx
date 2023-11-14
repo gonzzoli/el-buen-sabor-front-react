fc01e4c2fd177f8dbf826b962aea0ae2f9f91cc5
MartinaLuconi <martinaluconi21@gmail.com>
2023-11-14 09:59:57 -0300

    ..

--- src/componentes/Header.tsx
+++ src/componentes/Header.tsx
@@ -1,14 +1,16 @@
 import { faCartPlus, faBurger } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import Nav from 'react-bootstrap/Nav';
+import { useNavigate } from 'react-router-dom'
 const Header: React.FC = () => {
+  const Navigate = useNavigate();
   return (
     <header className="header">
       <div className="header-logo">
         <FontAwesomeIcon icon={faBurger} />
         <h3>El buen sabor</h3>
       </div>
-      <nav className="header-botones-links">
+      {/*<nav className="header-botones-links">
         <ul className="header-links">
           <li className="header-link">
             <a href="#">Inicio</a>
@@ -22,17 +24,25 @@ const Header: React.FC = () => {
           <li className="header-link">
             <a href="http://localhost:5173/rubros">Rubros</a>
           </li>
-        </ul>
+        </ul>*/}
+
+        <Nav className="me-auto">
+           
+            <Nav.Link onClick={() => Navigate('/')}>PaginaPrincipal</Nav.Link>
+            <Nav.Link onClick={() => Navigate('/rubros')}>Rubros</Nav.Link>
+            
+            
+        
         <div className="header-botones">
-          <button className="boton-secundario boton-carrito">
-            <FontAwesomeIcon icon={faCartPlus} />
-            <p>Carrito</p>
-          </button>
-          <button className="boton-primario">
-            <p>Iniciar Sesion</p>
-          </button>
+            <button className="boton-secundario boton-carrito">
+              <FontAwesomeIcon icon={faCartPlus} />
+              <p>Carrito</p>
+            </button>
+            <button className="boton-primario">
+              <p>Iniciar Sesion</p>
+            </button>
         </div>
-      </nav>
+        </Nav>
     </header>
   );
 };
