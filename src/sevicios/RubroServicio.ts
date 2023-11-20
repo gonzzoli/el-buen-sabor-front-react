0518b17f199d616fa986cbd466b9d5db52f93e37
MartinaLuconi <martinaluconi21@gmail.com>
2023-11-20 02:06:59 -0300

    ahora se ven los datos de la tabla

--- src/sevicios/RubroServicio.ts
+++ src/sevicios/RubroServicio.ts
@@ -1,5 +1,5 @@
 import { Rubro } from "../tipos/Rubro";
-const BASE_URL = 'https://ebsreact-persist.onrender.com';
+const BASE_URL = 'http://localhost:8080';
 export const RubroService = {
     
     /* Metodos ABM */
@@ -25,7 +25,7 @@ export const RubroService = {
     },
 
     modificarRubro:async (id:number, rubro: Rubro): Promise<Rubro> => {
-        const response = await fetch(`${BASE_URL}/api/v1/rubros/${id}` , {           //Esta ruta igual se puede mantener, ya que esta mapeando con BaseControllerImpl (Funciona)
+        const response = await fetch(`${BASE_URL}/api/v1/rubros/modificarRubro/${id}` , {           //Esta ruta igual se puede mantener, ya que esta mapeando con BaseControllerImpl (Funciona)
             method: "PUT",
             headers: {
                 'Content-Type': 'application/json'
@@ -35,13 +35,23 @@ export const RubroService = {
         const data = await response.json();
         return data;
     },
-
+//cambiarle el nombre
     buscarRubrosPorNombre:async (): Promise<Rubro[]> => {
-        const response = await fetch(`${BASE_URL}/api/v1/rubros/Nombre`);
+        const response = await fetch(`${BASE_URL}/api/v1/rubros`);
         const data = await response.json();
         
         return data;
     }
+
+   // mostrarRubro: async (id:number): Promise<void> => {
+    //    const response = await fetch(`${BASE_URL}/api/v1/rubros/${id}`, {         //Esta ruta igual se puede mantener, ya que esta mapeando con BaseControllerImpl
+          //  method: "GET"
+     //   });
+       // const rubro = await response.json();
+        //console.log("Rubro obtenido:", rubro);
+   // }
+
+
 
     
 
